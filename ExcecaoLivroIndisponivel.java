de2dec648d10de2de20359049dbaa2b1f83fa30f
Thiago Melo <78879698+ThiagoMarianoMelo@users.noreply.github.com>
2022-06-12 15:15:03 -0300

    Delete ExcecaoLivroIndisponivel.java

--- ExcecaoLivroIndisponivel.java
+++
@@ -1,8 +0,0 @@
-import java.util.List;
-
-public class ExcecaoLivroIndisponivel extends Exception{
-    
-    public ExcecaoLivroIndisponivel(List<Livro> livrosIndisponiveis){
-        super("os seguintes livros estao indisponiveis: "+ livrosIndisponiveis.toString());
-    }
-}
